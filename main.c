c5f4a563b18c5c8f66af714c03a382efa9a0cd81
Inamul07 <mohammedinamul87@gmail.com>
2024-03-05 15:51:38 +0530

    Update Makefile

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "./header_files/hashmap.h"
-#include "./header_files/dbllist.h"
 #include "./lru/lru.h"
 
 int main() {
@@ -12,9 +10,9 @@ int main() {
 
     lru_cache* cache = lru_init(3);
 
-    int seq[] = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2};
-    int size = sizeof(seq) / sizeof(seq[0]);
-    lru_put_array(cache, seq, size);
+    int pages[] = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2};
+    int size = sizeof(pages) / sizeof(pages[0]);
+    lru_put_array(cache, pages, size);
     lru_analysis(cache);
     lru_destroy(cache);
 
