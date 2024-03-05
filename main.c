30fe0fcbde829ca664005954ad998585e175fc3b
Inamul07 <mohammedinamul87@gmail.com>
2024-03-05 14:24:06 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,24 +4,9 @@
 # include <stdbool.h>
 # include "./header_files/hashmap.h"
 #include "./header_files/dbllist.h"
-#include "./lru/lru.h"
-#include "./clock/clock.h"
 
 
 int main() {
-
-    lru_cache* cache = lru_init(3);
-
-    int pages[] = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2};
-    int size = sizeof(pages) / sizeof(pages[0]);
-
-    lru_put_array(cache, pages, size);
-
-    lru_analysis(cache);
-
-    dbllist* list = dbllist_create();
-    dbllist_insert(list, 10);
-    dbllist_print(list);
 
     return 0;
 }
