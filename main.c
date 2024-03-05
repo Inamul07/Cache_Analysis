43e2debe72c90e021cae6c0eeafb4b1c0138e77a
Inamul07 <mohammedinamul87@gmail.com>
2024-03-05 15:24:24 +0530

    Update: lru.c

--- main.c
+++ main.c
@@ -12,28 +12,13 @@ int main() {
 
     lru_cache* cache = lru_init(3);
 
-    int seq[] = {10,15,25,10,30,50,20,35,30,20,50,70};
+    int seq[] = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2};
     int size = sizeof(seq) / sizeof(seq[0]);
     lru_put_array(cache, seq, size);
     lru_analysis(cache);
+    lru_destroy(cache);
 
     printf("\n");
-
-    lru_cache* cache2 = lru_init(3);
-
-    int loop[] = {1,5,7,10,12,1,5,7,3,8,1,5,7};
-    size = sizeof(loop) / sizeof(loop[0]);
-    lru_put_array(cache2, loop, size);
-    lru_analysis(cache2);
-
-    printf("\n");
-
-    lru_cache* cache3 = lru_init(3);
-
-    int rand[] = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2};
-    size = sizeof(rand) / sizeof(rand[0]);
-    lru_put_array(cache3, rand, size);
-    lru_analysis(cache3);
 
     return 0;
 }
