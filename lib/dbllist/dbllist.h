000e7dcd6618f987191cbcfc4e8fcc862a26a0cd
Inamul07 <mohammedinamul87@gmail.com>
2024-03-10 22:58:56 +0530

    Header Function Parameter Names

--- lib/dbllist/dbllist.h
+++ lib/dbllist/dbllist.h
@@ -4,22 +4,21 @@
 typedef struct linked_list_node Node;
 typedef struct dbllist_ dbllist;
 
-// CODE_REVIEW: add parameter names in both .c and .h files
-Node* node_create(int);
-int node_val(Node*);
-Node* node_create_with_ptrs(int, Node*, Node*);
-void node_destroy(Node*);
+Node* node_create(int data);
+int node_val(Node* node);
+Node* node_create_with_ptrs(int data, Node* prev, Node* next);
+void node_destroy(Node* node);
 
 dbllist* dbllist_create();
-int dbllist_size(dbllist*);
-void dbllist_insert_at_tail(dbllist*, int);
-void dbllist_insert_node_at_tail(dbllist*, Node*);
-void dbllist_move_node_to_tail(dbllist*, Node*);
-int dbllist_remove_head(dbllist*);
-int dbllist_remove_node(dbllist*, Node*);
-int dbllist_peek_head_val(dbllist*);
-int dbllist_peek_tail_val(dbllist*);
-void dbllist_print(dbllist*);
-void dbllist_destroy(dbllist*);
+int dbllist_size(dbllist* list);
+void dbllist_insert_at_tail(dbllist* list, int data);
+void dbllist_insert_node_at_tail(dbllist* list, Node* node);
+void dbllist_move_node_to_tail(dbllist* list, Node* node);
+int dbllist_remove_head(dbllist* list);
+int dbllist_remove_node(dbllist* list, Node* node);
+int dbllist_peek_head_val(dbllist* list);
+int dbllist_peek_tail_val(dbllist* list);
+void dbllist_print(dbllist* list);
+void dbllist_destroy(dbllist* list);
 
 #endif
--- lib/hashmap/myhashmap.h
+++ lib/hashmap/myhashmap.h
@@ -6,11 +6,11 @@
 typedef struct hashmap hashmap;
 struct pair;
 
-struct hashmap* hmap_create();
-void hmap_insert(struct hashmap*, int, void*);
-void* hmap_get(struct hashmap*, int);
-void* hmap_remove(struct hashmap*, int);
-int hmap_contains(struct hashmap*, int);
-void hmap_free(struct hashmap*);
+hashmap* hmap_create();
+void hmap_insert(hashmap* map, int key, void* value);
+void* hmap_get(hashmap* map, int key);
+void* hmap_remove(hashmap* map, int key);
+int hmap_contains(hashmap* map, int key);
+void hmap_free(hashmap* map);
 
 #endif
