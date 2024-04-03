a6b8e822ea35eac714e867652be79e713ac2df27
Inamul07 <mohammedinamul87@gmail.com>
2024-04-03 15:03:36 +0530

    Update: ARC comments

--- include/arc.h
+++ include/arc.h
@@ -14,6 +14,15 @@
  * Wnen there is a hit in B2, The p value is decreased (decrement by 1, if B2 >= B1, else decrement by a factor of B1 (i.e) B1/B2)
  * If the p value is greater, it allows more recent pages into the cache.
  * If the p value is smaller, it allows more frequent pages into the cache.
+ 
+ * ABOUT THE BUFFERS:
+ * When there is a large access of unrepeated pages, the size of the T1 buffer grows without affecting the pages in the T2 buffer.
+ * Similarly, Where there is repeated reference of certain pages those pages reside in T2 buffer.
+ * So, If the referenced pages are non repetitive, those pages are placed in the T1 buffer.
+ * When an accessed page is already residing in T1 buffer then that page is promoted to T2 buffer.
+ * If the refernced pages are repetive, Then those pages are placed in the T2 buffer.
+ * That reference page resides in T2, until it is evicted out due to lack of reference for a long peroid of time.
+ * Thus, The ARC algorithm adaptively changes the sizes of the buffers to achieve better cache hits.
 */
 
 #ifndef ARC_H
